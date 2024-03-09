dd073ba8725ef56893dd13f8e6e6de0aef7364bd
ceylan-palabiyik <148846654+ceylan-palabiyik@users.noreply.github.com>
2024-03-10 00:03:04 +0300

    ekle-sil

---
+++ İnput-ekle-sil/script.js
@@ -0,0 +1,24 @@
+const addbutton = document.getElementById("addToDo");
+const clearbutton = document.getElementById("clearToDo");
+const inputtext = document.getElementById("inputText");
+const todocontainer = document.getElementById("toDoContainer");
+
+addbutton.addEventListener("click", function () {
+  const paragraph = document.createElement("p");
+  paragraph.classList.add("paragraph-styling");
+  todocontainer.appendChild(paragraph);
+  paragraph.innerHTML = inputtext.value;
+  inputtext.value = "";
+
+  paragraph.addEventListener("click", function () {
+    paragraph.style.textDecoration = "line-through";
+  });
+
+  paragraph.addEventListener("dblclick", function () {
+    todocontainer.removeChild(paragraph);
+  });
+
+  clearbutton.addEventListener("click", function () {
+    paragraph.remove()
+  });
+});
